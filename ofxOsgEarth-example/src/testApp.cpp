bc1230a2bd21989d33280a28ed974e89dd303508
walter <info@kalwaltart.it>
2013-04-17 19:12:51 +0200

    first commit, just an example

---
+++ ofxOsgEarth-example/src/testApp.cpp
@@ -0,0 +1,95 @@
+#include "testApp.h"
+
+#include <osg/Geode>
+#include <osg/ShapeDrawable>
+#include <osg/Shape>
+#include <osgDB/ReadFile>
+//#include <osgDB/Util>
+#include <osgViewer/View>
+#include <osgGA/TrackballManipulator>
+//#include <osgEarthAnnotation/ModelNode>
+
+
+//--------------------------------------------------------------
+void testApp::setup(){
+    std::cout << "setup" << std::endl;
+
+    osg::Node* myModel = osgDB::readNodeFile("data/gdal_tiff.earth");
+
+   // osg::ShapeDrawable* drawable = new osg::ShapeDrawable(new osg::Box(osg::Vec3(0,0,0), 1.0f));
+   // osg::Geode* geode = new osg::Geode();
+    //geode->addDrawable(drawable);
+    //geode->addNode(myModel);
+
+    //getView()->setSceneData(geode);
+    getView()->setSceneData(myModel);
+    getView()->setCameraManipulator(new osgGA::TrackballManipulator());
+
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+     // std::cout << "update" << std::endl;
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+     // std::cout << "draw" << std::endl;
+    ofDrawBitmapString("ofxosgEarth Demo", 10, 10);
+    ofDrawBitmapString("fps: "+ofToString((ofGetFrameRate())), 10, 25);
+
+   // ofDrawBitmapString("'1': set window position\n'2': set window size\n'f': disable full screen \n'F': enable full screen\n's': show osg's stats-handler", 10,50);
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+    std::cout << "keypressed" << std::endl;
+    switch(key) {
+        case '1':
+            ofSetWindowPosition(100,100);
+            break;
+        case '2':
+            ofSetWindowShape(400, 300);
+            break;
+        case 'f':
+            ofSetFullscreen(false);
+            break;
+        case 'F':
+            ofSetFullscreen(true);
+            break;
+
+    }
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+  //   std::cout << "keyReleased" << std::endl;
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+  //   std::cout << "mouseMoved" << x << "/" << y << std::endl;
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+  //  std::cout << "mouseDragged" << x << "/" << y <<  std::endl;
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+ //   std::cout << "mousePressed" << x << "/" << y <<  std::endl;
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+  //  std::cout << "mouseReleased" << x << "/" << y <<  std::endl;
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+   // std::cout << "windowResized" << w << "x" << h <<  std::endl;
+}
+
